5c872449d5dc5d87bba3c06b45dc1b0913966f06
Gabe Rivera <gabriel.f.rivera.10@gmail.com>
2019-03-30 14:02:18 -0400

    testing store in Saved page

--- src/views/Saved.vue
+++ src/views/Saved.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <h1>Saved</h1>
-
+    <pre>
+      {{ stateParks }}
+    </pre>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 export default {
   name: "saved",
+  computed: {
+    ...mapState([
+      //change to saved when that logic is wired up:
+      'stateParks'
+    ])
+  }
 }
 </script>
 <style lang="sass">
